6514a799ae3a61a83b11adbc4f87ceaceafaf55d
KVRTeJ <bb.sssss@inbox.ru>
2025-02-09 11:48:54 +0500

    createt program-2 project. tested all project

--- program-2/main.cpp
+++ program-2/main.cpp
@@ -7,8 +7,8 @@
 int main() {
 
     for(;;) {
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    std::cout << "program-1" << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        std::cout << "program-2" << std::endl;
     }
 
     return 0;
